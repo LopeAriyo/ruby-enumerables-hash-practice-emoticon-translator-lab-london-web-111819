1979db8b1fa2f6c15a4e4405ef405f1c0fd6fdf0
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 20:30:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon (file_path, emoticon)
   output = emoticon_directory["get_emoticon"][emoticon]
 
   if output #if the output exists in the directory is true
-  
+
    output # returns the output
 
   if file_path.find  {|emoticon|}
@@ -36,7 +36,7 @@ def get_japanese_emoticon (file_path, emoticon)
   end
 
 
-  
+
 end
 
 def get_english_meaning (file_path, emoticon)
