3190463fb35ef1a2da8c5753d33b73ed61aec626
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 20:06:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
 def load_library(file_path)
 
 #return value is a hash {}
-# return value has two keys that point to inner hashes 
+# return value has two keys that point to inner hashes
 #{"get_meaning"=> {japanese_emoticon => meaning}, "get_emoticon" => {english_emoticon => japanese_equivalent}}
 
 p {}
@@ -14,7 +14,7 @@ def get_japanese_emoticon (file_path, emoticon)
   # code goes here
 
   load_library(file_path)
-  
+
 end
 
 def get_english_meaning (file_path, emoticon)
